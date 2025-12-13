0d9da5fa478c66ad931b2f53a9831c95fa21a065
namahu <neti0326@gmail.com>
2025-12-13 17:42:28 +0900

    #8 開始日と期限日がnullだった場合に「-」を表示するよう修正

--- taskapp/src/components/features/todo/TodoCard.vue
+++ taskapp/src/components/features/todo/TodoCard.vue
@@ -22,8 +22,8 @@ const { todo } = defineProps<{ todo: Todo }>()
           >{{ label.name }}</span
         >
       </div>
-      <span class="todo-card__start-date">開始日： {{ todo.start_date.toLocaleDateString() }}</span>
-      <span class="todo-card__due-date">期限： {{ todo.due_date.toLocaleDateString() }}</span>
+      <span class="todo-card__start-date">開始日： {{ todo.start_date ? todo.start_date.toLocaleDateString() : "-" }}</span>
+      <span class="todo-card__due-date">期限： {{ todo.due_date ? todo.due_date.toLocaleDateString() : "-" }}</span>
       <span class="todo-card__description">詳細： {{ todo.description }}</span>
     </div>
   </div>
