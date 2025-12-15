591e353d0b0f5d1b4f18207a2cb3aa4450e03c88
namahu <neti0326@gmail.com>
2025-12-15 12:44:43 +0900

    インポートパスを相対パスに変更

--- taskapp/backend/src/api/todo/todo.module.ts
+++ taskapp/backend/src/api/todo/todo.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TodoService } from './todo.service';
 import { TodoController } from './todo.controller';
-import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaModule } from '../../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
