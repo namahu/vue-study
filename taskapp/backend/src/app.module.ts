10924535fc3dbc262474baa44da6154dbc1bf795
namahu <neti0326@gmail.com>
2025-12-15 12:45:49 +0900

    PrismaServiceのインポートを削除

--- taskapp/backend/src/app.module.ts
+++ taskapp/backend/src/app.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './api/todo/todo.module';
-import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [TodoModule, PrismaModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService],
 })
 export class AppModule {}
