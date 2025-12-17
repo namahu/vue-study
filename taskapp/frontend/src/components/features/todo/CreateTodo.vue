af10836dbac6f90246bc3b987b8b92543a7ed9cf
namahu <neti0326@gmail.com>
2025-12-17 15:23:10 +0900

    Todo作成コンポーネントのデザイン作成

---
+++ taskapp/frontend/src/components/features/todo/CreateTodo.vue
@@ -0,0 +1,122 @@
+<script setup lang="ts">
+  import { ref } from "vue"
+
+  const isOpen = ref(false)
+
+  const toggleFormOpen = () => {
+    isOpen.value = !isOpen.value
+  }
+
+</script>
+
+<template>
+  <div class="create-todo-container">
+    <div class="create-todo__button">
+      <button type="button" @click="toggleFormOpen">新規Todo作成</button>
+    </div>
+    <div v-if="!isOpen" class="create-todo__form">
+      <form action="" method="post">
+        <h1>新規Todo作成</h1>
+        <div class="create-todo__form-item">
+          <label for="title">タイトル</label>
+          <input type="text" name="title" id="title" />
+        </div>
+        <div class="create-todo__form-item">
+          <label for="description">詳細</label>
+          <textarea name="description" id="description"></textarea>
+        </div>
+        <div class="create-todo__form-item">
+          <label for="start_date">開始日</label>
+          <input type="text" name="start_date" id="start_date" />
+        </div>
+        <div class="create-todo__form-item">
+          <label for="due_date">期限日</label>
+          <input type="text" name="due_date" id="due_date" />
+        </div>
+        <div class="create-todo__form-item">
+          <label for="labels">ラベル</label>
+          <select name="labels" id="labels">
+            <option value="none"></option>
+          </select>
+        </div>
+        <div class="form-button-container">
+          <button type="submit" class="button__submit">追加する</button>
+          <button type="button" class="button__cancel" @click="toggleFormOpen">キャンセルする</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="css">
+  .create-todo__button > button {
+    background: none;
+    background-color: #3A8FB7;
+    border: none;
+    border-radius: 8px;
+    color: #fffffb;
+    font-size: 1.2rem;
+    padding: 4px 16px;
+  }
+
+  .create-todo__form {
+    position: absolute;
+    top: 0;
+    right: 0;
+    background-color: #f8f9fa;
+    height: 100vh;
+    width: 24vw;
+  }
+
+  .create-todo__form form {
+    font-size: 1.5rem;
+    margin: 16px 32px;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .create-todo__form-item {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .create-todo__form-item:nth-child(n + 3) {
+    margin-top: 32px;
+  }
+
+  .create-todo__form-item *:nth-child(2) {
+    margin: 0 24px;
+  }
+
+  .create-todo__form-item input,
+  .create-todo__form-item textarea {
+    border: 1px solid #3e3e3e;
+    border-radius: 6px;
+    font-size: 1.5rem;
+    padding: 8px;
+  }
+
+  .form-button-container {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin-top: 64px;
+  }
+
+  .form-button-container button {
+    background: none;
+    border: none;
+    border-radius: 8px;
+    font-size: 1.5rem;
+    padding: 4px 16px;
+    cursor: pointer;
+  }
+
+  .form-button-container .button__submit {
+    background-color: blue;
+  }
+
+  .form-button-container .button__cancel {
+    background-color: red;
+  }
+</style>
