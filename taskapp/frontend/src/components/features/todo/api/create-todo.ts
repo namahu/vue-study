944d5c3d51da1c5a83792bf7a98ab7863b386946
namahu <neti0326@gmail.com>
2025-12-18 16:33:30 +0900

    戻り値の型を追加

--- taskapp/frontend/src/components/features/todo/api/create-todo.ts
+++ taskapp/frontend/src/components/features/todo/api/create-todo.ts
@@ -1,9 +1,9 @@
 import { api } from "@/composables/useAPI";
-import type { CreateTodoType } from "@/types";
+import type { CreateTodoType, Todo } from "@/types";
 
 export const useCreateTodo = async (newTodo: CreateTodoType) => {
   try {
-    const response = await api.post("api/todo", newTodo);
+    const response = await api.post<Todo>("api/todo", newTodo);
     return response.data;
   } catch (error) {
     console.error("Todoの作成に失敗しました。", error);
