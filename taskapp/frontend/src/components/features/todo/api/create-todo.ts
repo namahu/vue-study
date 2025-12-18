9c00e3dbc0b2c03804a5c73b464acac4a3309393
namahu <neti0326@gmail.com>
2025-12-18 16:20:58 +0900

    response.dataのリターンとエラーハンドリング追加

--- taskapp/frontend/src/components/features/todo/api/create-todo.ts
+++ taskapp/frontend/src/components/features/todo/api/create-todo.ts
@@ -2,6 +2,11 @@ import { api } from "@/composables/useAPI";
 import type { CreateTodoType } from "@/types";
 
 export const useCreateTodo = async (newTodo: CreateTodoType) => {
-  const response = await api.post("api/todo", newTodo);
-  console.log(response);
+  try {
+    const response = await api.post("api/todo", newTodo);
+    return response.data;
+  } catch (error) {
+    console.error("Todoの作成に失敗しました。", error);
+    throw error;
+  }
 }
